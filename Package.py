b5ade637d41d711d854b5d30479a955d634deb07
drewlew13 <lewis446@purdue.edu>
2021-11-16 22:53:17 -0500

    more api

--- Package.py
+++ Package.py
@@ -1,16 +1,12 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, request
 from ApplicationService import *
-from flask_marshmallow import Marshmallow
 from google.cloud import storage
 #from your_orm import Model, Column, Integer, String, DateTime
 import sys
 
 app = Flask(__name__)
-ma = Marshmallow(app)
 
-
-@app.route("/package/<id>")
+@app.route("/package/<id>", method='GET')
 def getPackage(id):
         storageClient = storage.Client.from_service_account_json("./google-cloud-creds.json")
         bucketName = os.getenv("BUCKET_NAME")
@@ -22,8 +18,36 @@ def getPackage(id):
         else:
             return {'metadata': "None"}, 400
 
+@app.route("/package/<id>", method='PUT')
+def putPackage(id):
+    pass
 
-#api.add_resource(Package, '/package/<id>')
+@app.route("/package/<id>", method='DELETE')
+def delPackageVers(id):
+    pass
+
+@app.route("/package/<id>/rate", method="GET")
+def ratePackage(id):
+    pass
+
+@app.route("/package/byName/<name>", method='GET')
+def getPackageByName(name):
+    pass
+
+@app.route("/package/byName/<name>", method='DELETE')
+def delAllPackageVers(name):
+    pass
+
+@app.route("/package", method='POST')
+def createPackage():
+    pass
+
+@app.route("/packages", method='POST')
+def createPackage():
+    offset = request.args.get('offset')
+    return {'offset': {"offsetAct": offset}}
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)

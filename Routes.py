3bacb9be1b7152a22f4e60c943cdacef00eda003
drewlew13 <lewis446@purdue.edu>
2021-12-01 16:54:31 -0500

    update api

--- Routes.py
+++ Routes.py
@@ -203,7 +203,6 @@ def createPackage():
             files = []
             files.append(newFile)
             appService.upload(files)
-            raise Exception('Uploads bucket blob!')
             
             packageList[data["metadata"]["ID"]] = data["metadata"]
             actionHistory[data["metadata"]["ID"]].append((datetime.now(), "CREATE"))

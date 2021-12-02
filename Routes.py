bbb901be47d50329bfd4ff7f20f30048dc85e6d4
drewlew13 <lewis446@purdue.edu>
2021-12-01 23:08:12 -0500

    update api

--- Routes.py
+++ Routes.py
@@ -65,6 +65,8 @@ def getPackage(id):
             except:
                 repoUrl = "No URL Found."
 
+            return {"ID": id}
+
             actionHistory[id].append((datetime.now(), "GET"))
         
             return {'metadata': {"Name": packageList[id]["Name"], "Version": packageList[id]["Version"], "ID": id}, "data": {"Content": encodedStr, "URL": repoUrl, "JSProgram": "if (process.argv.length === 7) {\nconsole.log('Success')\nprocess.exit(0)\n} else {\nconsole.log('Failed')\nprocess.exit(1)\n}\n"}}, 200

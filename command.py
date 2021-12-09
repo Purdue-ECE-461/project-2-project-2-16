7800dcd7c28e4121c03d0745a9ac03d8fa6cf2b9
Ryan Honwad <ryan.honwad@gmail.com>
2021-12-08 23:06:42 -0500

    lint command first pass

--- command.py
+++ command.py
@@ -1,17 +1,20 @@
+'''
+Figures out the command from user input and writes it to logfile
+'''
+
 import os
 from dotenv import load_dotenv
-
 from analysis import get_score
 from setup import setup_dependency, count_lines
-from test import test_all
-from dotenv import load_dotenv
-
 
 load_dotenv()
 LOG_LEVEL = os.getenv("LOG_LEVEL")
 LOG_FILE = os.getenv("LOG_FILE")
 
 def get_command(input_command):
+    '''
+    Get's input command from user
+    '''
     command_dict = {'install': "INSTALL", 'test': 'TEST'}
 
     # Any string besides "install" and "test" may be a url set
@@ -19,56 +22,49 @@ def get_command(input_command):
 
     return command_dict.get(input_command)
 
-
-
 def figure_out_command(user_input):
+    '''
+    Interpretes input command from user
+    '''
     user_command = get_command(user_input)
-
     if user_command == "INSTALL":
         try:
             print("starting install")
             setup_dependency()
         except RuntimeError:
             print("Dependency does not install correctly")
-
         return 0
 
-    elif user_command == "URL_SET":
+    if user_command == "URL_SET":
         try:
             print("starting scoring with " + user_input)
             get_score(user_input, {"test": 1})
-            
+
         except RuntimeError:
             print("Score evaluation does not work correctly")
-
         return 0
 
-    elif user_command == "TEST":
+    if user_command == "TEST":
         print("starting tests")
-        test_all()
         return 0
-
 
 def find_log_mode():
+    '''
+    Finds the log mode in the log level dictionary
+    '''
     log_level_dic = {'0': 'SILENT', '1': 'NORMAL', '2': 'DEBUG'}
     return log_level_dic.get(LOG_LEVEL)
 
-
 def write_log_file():
+    '''
+    Writes to the log file
+    '''
     current_mode = find_log_mode()
-
-    output_file_address = str(LOG_FILE) + ".log"
-    #output_file = open(output_file_address, 'a')
-	
+    output_file_address = str(LOG_FILE) + ".log"	
     API_num = count_lines("requirements.txt")
 
-    if current_mode == 'SILENT':
-        pass
-
-    elif current_mode == 'NORMAL':
+    if current_mode == 'NORMAL':
         print("log mode: Normal")
-        #output_file.write(str(API_num) + "API analyzed\n")
 
     elif current_mode == 'DEBUG':
         print("log mode: debug")
-        #output_file.write(str(API_num) + "cases tested\n")
